ca5698edc3fa9eda7ea4a0fee1052e6c1e435f35
elbanko <ankus.benjamin@gmail.com>
2018-06-26 18:00:18 -0400

    update 10

--- nmbb/oldsettings.py
+++ nmbb/oldsettings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['nomorebillabesbeta.herokuapp.com', 'herokuapp.com']
+ALLOWED_HOSTS = ['nmbb.herokuapp.com', 'herokuapp.com']
 
 
 # Application definition
