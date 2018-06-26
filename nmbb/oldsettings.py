fe9c9ebf1fc74e71a5f3d6127432b87191b9b16f
elbanko <ankus.benjamin@gmail.com>
2018-06-26 17:38:27 -0400

    Update 9

--- nmbb/oldsettings.py
+++ nmbb/oldsettings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['nomorebillabesbeta.herokuapp.com', 'herokuapp.com']
 
