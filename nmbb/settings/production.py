0a62a2f85317c4c76214a91d71c7a3b4747d2746
elbanko <ankus.benjamin@gmail.com>
2018-06-27 00:11:36 -0400

    Update 18

--- nmbb/settings/production.py
+++ nmbb/settings/production.py
@@ -66,16 +66,17 @@ WSGI_APPLICATION = 'nmbb.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-# import dj_database_url
-# db_from_env = dj_database_url.config()
-# DATABASES['default'].update(db_from_env)
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'] = update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
