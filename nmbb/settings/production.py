d162f39414eaf373be4ebd1dcf0bdaf29f6cbe30
elbanko <ankus.benjamin@gmail.com>
2018-06-26 23:47:15 -0400

    update 17

--- nmbb/settings/production.py
+++ nmbb/settings/production.py
@@ -1,7 +1,5 @@
 import os
-import psycopg2
 import dj_database_url
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -67,11 +65,8 @@ WSGI_APPLICATION = 'nmbb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASE_URL = os.environ.get('HEROKU_POSTGRESQL_ROSE_URL')
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-DATABASES = {'default': {'ENGINE': dj_database_url.config(conn_max_age=600, ssl_require=True)}}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
